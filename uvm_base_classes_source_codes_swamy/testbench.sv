541acef18b6d4b08b341b050a5b038af99a46e72
dharaswamy <dharaswamy143y@gmail.com>
2025-06-24 20:36:06 +0530

    uvm base classes source codes , eda link https://edaplayground.com/x/viML

---
+++ uvm_base_classes_source_codes_swamy/testbench.sv
@@ -0,0 +1,11 @@
+// Eda link : https://edaplayground.com/x/viML
+
+// ( swamy ) please copy the code but don't change/modify the code here.
+
+//=============================================================================================================================
+// TOPIC NAME: UVM BASE CLASSES SOURCE CODES .
+
+// i.uvm_drive
+// ii.uvm_subscribe.
+
+//=============================================================================================================================
